a41d292af64871679e20ddab2c1c10e383729763
Francois Blackburn <blackburnfrancois@gmail.com>
2020-02-11 19:45:31 -0500

    add device node

--- nodes/config.js
+++ nodes/config.js
@@ -1,6 +1,9 @@
 module.exports = function(RED) {
   const fetch = require('node-fetch');
 
+  let nodes = {};
+  let callbacks = [];
+
   function HubitatConfigNode(config) {
     RED.nodes.createNode(this,config);
     this.name = config.name;
@@ -12,6 +15,47 @@ module.exports = function(RED) {
 
     const scheme = ((this.usetls) ? 'https': 'http');
     this.base_url = `${scheme}://${this.host}:${this.port}/apps/api/${this.api_id}`;
+
+    let node = this;
+
+    if ((!node.host) || (!node.port) || (!node.token) || (!node.api_id)) {
+      return;
+    }
+
+    node.getDevice = async function(device_id, type) {
+      const url = `${node.base_url}/devices/${device_id}?access_token=${node.token}`;
+      const options = {method: 'GET'};
+      try {
+        const response = await fetch(url, options);
+        device = await response.json();
+      }
+      catch(err) {
+        console.log("unable to fetch device: " + device_id);
+        return;
+      }
+      console.log("HubitatConfigNode: Device:");
+      console.log(device);
+      return device;
+    };
+
+    node.unregisterCallback = function(parent, device_id, callback) {
+      if (callbacks[device_id]) {
+        callbacks[device_id].filter( (c) => c !== callback );
+      }
+    };
+
+    node.registerCallback = function(parent, device_id, callback) {
+      if (!(device_id in callbacks)) {
+        callbacks[device_id] = [];
+      }
+
+      callbacks[device_id].push({
+        parent: parent,
+        callback: callback
+      });
+    };
+
+    nodes[node.base_url] = node;
   }
 
   RED.nodes.registerType("hubitat config", HubitatConfigNode);
@@ -63,4 +107,25 @@ module.exports = function(RED) {
       res.send(err);
     }
   });
+
+  RED.httpAdmin.post('/hubitat/webhook', function(req,res){
+
+    console.log("POST /hubitat/webhook with body:");
+    console.log(req.body);
+    if (!req.body.content) {
+      res.sendStatus(400);
+      return;
+    }
+
+    const callback = callbacks[req.body.content["deviceid"]];
+
+    if(callback){
+      callback.forEach( (c) => {
+        c.callback.call(c.parent, req.body.content);
+      });
+    }
+
+    res.sendStatus(204);
+  });
+
 }
---
+++ nodes/device.js
@@ -0,0 +1,107 @@
+module.exports = function(RED) {
+  const fetch = require('node-fetch');
+
+  function castHubitatValue(dataType, value) {
+    switch(dataType) {
+      case "STRING":
+        return value;
+      case "NUMBER":
+        return parseFloat(value);
+      case "BOOL":
+        return value == "true";
+      default:
+        console.warn("Unable to cast to dataType. Open an issue to report back the following output:");
+        console.warn(dataType);
+        console.warn(value);
+        return;
+    }
+  }
+
+  function HubitatDeviceNode(config) {
+    RED.nodes.createNode(this, config);
+
+    this.hubitat = RED.nodes.getNode(config.server);
+    this.name = config.name;
+    this.deviceId = config.deviceId;
+    this.currentAttributes = undefined;
+
+    let node = this;
+
+    if (!node.hubitat) {
+      console.log("HubitatDeviceNode: Hubitat server not configured");
+      return;
+    }
+
+    node.reportStatus = function(original, send) {
+      let msg = {
+        payload: {
+          // add deviceId
+          attributes: node.currentAttributes
+        }
+      };
+      original.payload = msg.payload;
+      Object.assign(msg, original);
+
+      // TODO check why we need this
+      if (send === undefined) { 
+        this.send(msg);
+      } else {
+        send(msg);
+      }
+    }
+
+    node.updateStatus = (currentAttributes) => {
+      this.currentAttributes = currentAttributes;
+      this.reportStatus({});
+    }
+
+    const callback = (event) => {
+      console.debug("Device(" + node.name + "): Callback called");
+      console.debug(event);
+      if (this.currentAttributes === undefined) {
+        node.status({fill:"red", shape:"dot", text:"Uninitialized"});
+        console.warn("Device(" + node.name + "): Uninitialized");
+        return;
+      }
+
+      let found = false;
+      this.currentAttributes.forEach( (attribute) => {
+        if (event["name"] === attribute["name"]) {
+          attribute["currentValue"] = castHubitatValue(attribute["dataType"], event["value"]);
+          node.status({});
+          found = true;
+        }
+      });
+
+      if (!found) {
+        node.status({fill:"red", shape:"dot", text:"Unknown event: " + event["name"]});
+        return;
+      }
+      this.reportStatus({});
+    }
+
+    node.hubitat.registerCallback(node, node.device, callback);
+
+    node.hubitat.getDevice(node.deviceId).then( (device) => {
+      console.debug("Device(" + node.name + "): Status refreshed");
+      node.updateStatus(device.attributes);
+      node.status({});
+    }).catch( err => {
+      console.log(err);
+      node.status({fill:"red", shape:"dot", text:"Uninitialized"});
+    });
+
+    node.on('input', async function(msg, send, done) {
+      console.debug("HubitatDeviceNode: Input received");
+      node.reportStatus(msg, send);
+      done();
+    });
+
+    node.on('close', function() {
+      console.debug("HubitatDeviceNode: Closed");
+      node.hubitat.unregisterCallback(node, node.deviceId, callback);
+    });
+  }
+
+  RED.nodes.registerType("hubitat device", HubitatDeviceNode);
+}
