3cd65bb9247bc9d0865682e4beb21c87cda8cf37
Francois Blackburn <blackburnfrancois@gmail.com>
2020-03-21 13:07:59 -0400

    device: add on event received test

--- test/nodes/device_spec.js
+++ test/nodes/device_spec.js
@@ -1,9 +1,21 @@
 /* eslint-disable no-unused-vars */
 const helper = require('node-red-node-test-helper');
+const configNode = require('../../nodes/config.js');
 const deviceNode = require('../../nodes/device.js');
 
-
 describe('Hubitat Device Node', () => {
+  const defaultConfigNode = {
+    id: 'n0',
+    type: 'hubitat config',
+    name: 'test config name',
+    usetls: false,
+    host: 'localhost',
+    port: 10234,
+    token: '1234-abcd',
+    appId: 1,
+    nodeRedServer: 'localhost',
+    webhookPath: '/hubitat/webhook',
+  };
   const defaultDeviceNode = {
     id: 'n1',
     type: 'hubitat device',
@@ -31,6 +43,33 @@ describe('Hubitat Device Node', () => {
       } catch (err) {
         done(err);
       }
+    });
+  });
+  it('should send event when event received', (done) => {
+    const flow = [
+      defaultConfigNode,
+      { ...defaultDeviceNode, wires: [['n2']] },
+      { id: 'n2', type: 'helper' },
+    ];
+    const hubitatEvent = {
+      deviceId: '42',
+      name: 'testAttribute',
+      value: 'new-value',
+    };
+    helper.load([deviceNode, configNode], flow, () => {
+      const n0 = helper.getNode('n0');
+      const n1 = helper.getNode('n1');
+      const n2 = helper.getNode('n2');
+      n1.currentAttributes = [{ name: 'testAttribute', value: 'old-value' }];
+      n2.on('input', (msg) => {
+        try {
+          msg.should.have.property('payload', { ...hubitatEvent, currentValue: hubitatEvent.value });
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n0.hubitatEvent.emit('device', hubitatEvent);
     });
   });
 });
