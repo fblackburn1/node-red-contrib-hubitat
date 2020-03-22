0a90b32f014bda2378fb0e886822be1924b0a73f
Francois Blackburn <blackburnfrancois@gmail.com>
2020-03-22 07:43:17 -0400

    mode: add on event received test

--- test/nodes/mode_spec.js
+++ test/nodes/mode_spec.js
@@ -1,7 +1,20 @@
 const helper = require('node-red-node-test-helper');
+const configNode = require('../../nodes/config.js');
 const modeNode = require('../../nodes/mode.js');
 
 describe('Hubitat Mode Node', () => {
+  const defaultConfigNode = {
+    id: 'n0',
+    type: 'hubitat config',
+    name: 'test config name',
+    usetls: false,
+    host: 'localhost',
+    port: 10234,
+    token: '1234-abcd',
+    appId: 1,
+    nodeRedServer: 'localhost',
+    webhookPath: '/hubitat/webhook',
+  };
   const defaultModeNode = {
     id: 'n1',
     type: 'hubitat mode',
@@ -25,6 +38,34 @@ describe('Hubitat Mode Node', () => {
       } catch (err) {
         done(err);
       }
+    });
+  });
+  it('should send event when event received', (done) => {
+    const flow = [
+      defaultConfigNode,
+      { ...defaultModeNode, wires: [['n2']] },
+      { id: 'n2', type: 'helper' },
+    ];
+    const hubitatEvent = {
+      name: 'mode',
+      value: 'new-value',
+      displayName: 'My Hub',
+      descriptionText: 'New Value',
+    };
+    helper.load([modeNode, configNode], flow, () => {
+      const n0 = helper.getNode('n0');
+      const n1 = helper.getNode('n1');
+      const n2 = helper.getNode('n2');
+      n1.currentMode = 'old-value';
+      n2.on('input', (msg) => {
+        try {
+          msg.should.have.property('payload', hubitatEvent);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n0.hubitatEvent.emit('mode', hubitatEvent);
     });
   });
 });
