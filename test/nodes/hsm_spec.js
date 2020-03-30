44679dafefd28b4cac32b82be284bf3519523973
Francois Blackburn <blackburnfrancois@gmail.com>
2020-03-30 17:37:14 -0400

    hsm: add some tests

--- test/nodes/hsm_spec.js
+++ test/nodes/hsm_spec.js
@@ -1,13 +1,25 @@
 const helper = require('node-red-node-test-helper');
+const configNode = require('../../nodes/config.js');
 const hsmNode = require('../../nodes/hsm.js');
 
 describe('Hubitat HSM Node', () => {
-  const defaultHSMNode = {
+  const defaultConfigNode = {
+    id: 'n0',
+    type: 'hubitat config',
+    name: 'test config name',
+    usetls: false,
+    host: 'localhost',
+    port: 10234,
+    token: '1234-abcd',
+    appId: 1,
+    nodeRedServer: 'localhost',
+    webhookPath: '/hubitat/webhook',
+  };
+  const defaultHsmNode = {
     id: 'n1',
     type: 'hubitat hsm',
     name: 'test hsm name',
     server: 'n0',
-    command: 'HSMcmd',
     sendEvent: true,
   };
 
@@ -16,13 +28,63 @@ describe('Hubitat HSM Node', () => {
   });
 
   it('should be loaded', (done) => {
-    const flow = [defaultHSMNode];
+    const flow = [defaultHsmNode];
     helper.load(hsmNode, flow, () => {
       const n1 = helper.getNode('n1');
       try {
         n1.should.have.property('name', 'test hsm name');
         n1.should.have.property('sendEvent', true);
-        n1.should.have.property('command', 'HSMcmd');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+  it('should send event when event received', (done) => {
+    const flow = [
+      defaultConfigNode,
+      { ...defaultHsmNode, wires: [['n2']] },
+      { id: 'n2', type: 'helper' },
+    ];
+    const hubitatEvent = {
+      name: 'hsmStatus',
+      value: 'armNight',
+      displayName: 'Hubitat',
+      descriptionText: null,
+    };
+    helper.load([hsmNode, configNode], flow, () => {
+      const n1 = helper.getNode('n1');
+      const n2 = helper.getNode('n2');
+      n1.currentHsm = 'disarmed';
+      n2.on('input', (msg) => {
+        try {
+          msg.should.have.property('payload', hubitatEvent);
+          done();
+        } catch (err) {
+          done(err);
+        }
+      });
+      n1.hubitat.hubitatEvent.emit('hsm', hubitatEvent);
+    });
+  });
+  it('should use dot shape icon when send event', (done) => {
+    const flow = [{ ...defaultHsmNode, sendEvent: true }];
+    helper.load(hsmNode, flow, () => {
+      const n1 = helper.getNode('n1');
+      try {
+        n1.should.have.property('shape', 'dot');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+  it('should use ring shape icon when send event', (done) => {
+    const flow = [{ ...defaultHsmNode, sendEvent: false }];
+    helper.load(hsmNode, flow, () => {
+      const n1 = helper.getNode('n1');
+      try {
+        n1.should.have.property('shape', 'ring');
         done();
       } catch (err) {
         done(err);
