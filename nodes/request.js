29497e6b7528acbba5da4198761bb8616f6ff3c9
Francois Blackburn <blackburnfrancois@gmail.com>
2020-04-15 19:46:55 -0400

    add request node

---
+++ nodes/request.js
@@ -0,0 +1,48 @@
+/* eslint-disable global-require */
+module.exports = function HubitatRequestModule(RED) {
+  const fetch = require('node-fetch');
+
+  function HubitatRequestNode(config) {
+    RED.nodes.createNode(this, config);
+
+    this.hubitat = RED.nodes.getNode(config.server);
+    this.path = config.path;
+    const node = this;
+
+    if (!node.hubitat) {
+      node.error('Hubitat server not configured');
+      return;
+    }
+
+    node.on('input', async (msg, send, done) => {
+      node.status({ fill: 'blue', shape: 'dot', text: 'requesting' });
+
+      const path = msg.path || node.path;
+      if (!path) {
+        node.status({ fill: 'red', shape: 'ring', text: 'undefined path' });
+        done('undefined path');
+        return;
+      }
+      const url = `${node.hubitat.baseUrl}/${path}?access_token=${node.hubitat.token}`;
+      const options = { method: 'GET' };
+
+      try {
+        const response = await fetch(url, options);
+        if (response.status >= 400) {
+          node.status({ fill: 'red', shape: 'ring', text: 'response error' });
+          done(await response.text());
+          return;
+        }
+        const output = { ...msg, payload: await response.json() };
+        node.status({});
+        send(output);
+        done();
+      } catch (err) {
+        node.status({ fill: 'red', shape: 'ring', text: err.code });
+        done(err);
+      }
+    });
+  }
+
+  RED.nodes.registerType('hubitat request', HubitatRequestNode);
+};
