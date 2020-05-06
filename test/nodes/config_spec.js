901d2af1f4e128f9eaf956b06b0093afb89397fc
Francois Blackburn <blackburnfrancois@gmail.com>
2020-05-06 18:02:33 -0400

    add config basic test

---
+++ test/nodes/config_spec.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-vars */
+const helper = require('node-red-node-test-helper');
+const configNode = require('../../nodes/config.js');
+
+
+describe('Hubitat Command Node', () => {
+  const defaultConfigNode = {
+    id: 'n0',
+    type: 'hubitat config',
+    name: 'test config name',
+    usetls: false,
+    host: 'localhost',
+    port: 10234,
+    appId: 1,
+    nodeRedServer: 'localhost',
+    webhookPath: '/hubitat/webhook',
+  };
+
+  it('should be loaded', (done) => {
+    const flow = [defaultConfigNode];
+    helper.load(configNode, flow, () => {
+      const n0 = helper.getNode('n0');
+      try {
+        n0.should.have.property('name', 'test config name');
+        n0.should.have.property('usetls', false);
+        n0.should.have.property('host', 'localhost');
+        n0.should.have.property('port', 10234);
+        n0.should.have.property('appId', 1);
+        n0.should.have.property('nodeRedServer', 'localhost');
+        n0.should.have.property('webhookPath', '/hubitat/webhook');
+        done();
+      } catch (err) {
+        done(err);
+      }
+    });
+  });
+});
