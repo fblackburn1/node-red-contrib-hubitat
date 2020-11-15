48c03d21c55c7c51f7e33dc60a466b38d7c9135b
Francois Blackburn <blackburnfrancois@gmail.com>
2020-11-15 09:08:03 -0500

    add deviceId to error logs

--- nodes/utils/done-with-id.js
+++ nodes/utils/done-with-id.js
@@ -1,5 +1,8 @@
 function doneWithId(node, done, msg) {
   let message = msg;
+  if (node.deviceId) {
+    message = `[deviceId:${node.deviceId}] ${message}`;
+  }
   if (node.name) {
     message = `[${node.id}] ${message}`;
   }
